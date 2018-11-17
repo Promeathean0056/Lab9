7ed57ede2d69ab9e062b9ed76811243b7ae0e026
Promeathean0056 <34377463+Promeathean0056@users.noreply.github.com>
2018-11-17 17:03:03 -0500

    Add files via upload

---
+++ Lab 9/EliWhitmanLab9/EliWhitmanLab9/MyMath.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliWhitmanLab9
+{
+    class MyMath
+    {
+        double result;
+        double operand1 = 40;
+        double operand2 = 2;
+        
+
+        public void Multiply(double operand1, double operand2)
+        {
+            result = operand1 * operand2;
+            
+        }
+        public void Divide(double operand1, double operand2)
+        {
+            result = operand1 / operand2;
+           
+        }
+        public void Subtract(double operand1, double operand2)
+        {
+            result = operand1 - operand2;
+           
+        }
+        public void Add(double operand1, double operand2)
+        {
+            result = operand1 + operand2;
+            
+
+        }
+
+        public void GetResult(double result)
+        {
+            Console.WriteLine("the anwser is... " + result);
+        }
+
+
+    }
+}
---
+++ Lab 9/EliWhitmanLab9/EliWhitmanLab9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EliWhitmanLab9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyMath CalculateAdd = new MyMath();
+            MyMath CalculateSub = new MyMath();
+            MyMath CalculateMulti = new MyMath();
+            MyMath CalculateDiv = new MyMath();
+            //You can edit the values in MyMath.  I chose 40 and 2 as a default.
+           
+
+
+
+
+        }
+    }
+}
